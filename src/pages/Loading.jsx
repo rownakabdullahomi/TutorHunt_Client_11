6988b908806b44553477797674a1a47589b2429d
Rownak Abdullah <rownakabdullahomi@gmail.com>
2024-12-26 02:55:24 +0600

    react spinner added for loading spinner

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,11 +1,16 @@
-import loadingSpinner from "../assets/800.gif"
+// import loadingSpinner from "../assets/800.gif"
+import BeatLoader from "react-spinners/BeatLoader";
 
 const Loading = () => {
     return (
+        // <div className="flex min-h-screen justify-center items-center">
+        //     <span className="">
+        //         <img src={loadingSpinner} alt="" />
+        //     </span>
+        // </div>
+
         <div className="flex min-h-screen justify-center items-center">
-            <span className="">
-                <img src={loadingSpinner} alt="" />
-            </span>
+        <BeatLoader color="#38D6B7" size={80} />
         </div>
     );
 };
