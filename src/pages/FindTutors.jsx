aa20c2fecad99de73bfdb56862e6d93b84a98cad
Rownak Abdullah <rownakabdullahomi@gmail.com>
2024-12-26 20:57:12 +0600

    pagination updated

--- src/pages/FindTutors.jsx
+++ src/pages/FindTutors.jsx
@@ -151,9 +151,10 @@ const FindTutors = () => {
             className="select select-bordered"
           >
             <option value={6}>6</option>
-            <option value={8}>8</option>
-            <option value={9}>9</option>
             <option value={10}>10</option>
+            <option value={15}>15</option>
+            <option value={20}>20</option>
+            <option value={25}>25</option>
           </select>
         </div>
       </Slide>
